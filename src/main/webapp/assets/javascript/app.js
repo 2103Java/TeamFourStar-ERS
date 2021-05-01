1b291f26b6dbbdd2a8a7c14dba87867caadbfab5
Coach-BWise <wisebry11@gmail.com>
2021-04-30 20:06:00 -0400

    starting AJAX

--- src/main/webapp/assets/javascript/app.js
+++ src/main/webapp/assets/javascript/app.js
@@ -23,3 +23,14 @@ function isEmail(email) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
 
+// function login(userName, password) {
+//   var xhttp = new XMLHttpRequest();    
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       xhttp.response
+//    }
+//   };
+//   xhttp.open("GET", "/TeamFourStar-ERS/ers/login", true);
+//   xhttp.send();
+// }
+
